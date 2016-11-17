5fba578910d33c44955abd967127e60a35e085ed
ksu <ksu@ksutekiiMac.local>
2016-11-17 19:40:41 +0800

    Initial Commit

---
+++ text1117/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  text1117
+//
+//  Created by ksu on 2016/11/17.
+//  Copyright © 2016年 ksu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
